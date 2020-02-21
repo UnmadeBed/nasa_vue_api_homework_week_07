9f51336135ff8078b8f1f9a4890650bbe1fd45f9
unmadebed <kevin.archer@live.com>
2020-02-21 11:46:54 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,39 @@
-<template>
+
+
+<template lang="html">
+  <div>
+    <h1>NASA Photo of The Day</h1>
+    <p>{{ nasa }}</p>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      nasa: []
+    }
+  },
+  mounted (){
+    fetch('https://api.nasa.gov/planetary/apod?api_key=C0ehDJAti1cLdlnjQciOknJg4WMAeOBqcpOL1G4a')
+    .then(res => res.json())
+    .then(nasa => this.nasa = nasa)
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
+
+
+
+
+
+
+<!-- <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -25,4 +60,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
