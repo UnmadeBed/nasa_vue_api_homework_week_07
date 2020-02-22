679818cdf6ab0c1982af1e4bce492d00239f8768
unmadebed <kevin.archer@live.com>
2020-02-22 18:01:52 +0000

    image search component placeholder added

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,30 @@
   <div>
     <h1>NASA Photo of The Day</h1>
     <!-- Move this into a seperate image view component -->
-    <img :src="nasa.url" :alt="nasa.url">
+    <!-- <img :src="nasa.url" :alt="nasa.url"> MOVED -->
+    <input type="date" v-model="search"/>
+    <image-view :nasa="nasa" ></image-view>
 
   </div>
 
 </template>
 
 <script>
+import ImageView from "./components/ImageView.vue"
 export default {
   data(){
     return {
-      nasa: []
+      nasa: [],
+      search:""
     }
   },
   mounted (){
     fetch('https://api.nasa.gov/planetary/apod?api_key=C0ehDJAti1cLdlnjQciOknJg4WMAeOBqcpOL1G4a')
     .then(res => res.json())
     .then(nasa => this.nasa = nasa)
+  },
+  components: {
+    "image-view": ImageView
   }
 
 }
--- src/components/ImageView.vue
+++ src/components/ImageView.vue
@@ -1,12 +1,15 @@
 <template lang="html">
   <div>
-
+    <img :src="nasa.url" :alt="nasa.url">
   </div>
-  
+
 </template>
 
 <script>
+
 export default {
+  name: 'image-view',
+  props: ['nasa']
 }
 </script>
 
