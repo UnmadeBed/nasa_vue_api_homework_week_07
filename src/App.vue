955199a81d60eabb3105a4d2d975194595769313
unmadebed <kevin.archer@live.com>
2020-02-22 22:01:58 +0000

    edit date search function

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,45 @@
-
-
 <template lang="html">
   <div>
     <h1>NASA Photo of The Day</h1>
     <!-- Move this into a seperate image view component -->
     <!-- <img :src="nasa.url" :alt="nasa.url"> MOVED -->
-    <input type="date" v-model="search"/>
+    <input v-model="selectedDate" type="date">
+    <!-- <button @click="callApi">Get new image</button> -->
     <image-view :nasa="nasa" ></image-view>
-
   </div>
 
 </template>
 
 <script>
-import ImageView from "./components/ImageView.vue"
+import ImageView from './components/ImageView.vue'
 export default {
-  data(){
+  data() {
     return {
-      nasa: [],
-      search:""
-    }
+      nasa:[],
+      selectedDate: '2020-02-20'
+    };
   },
-  mounted (){
-    fetch('https://api.nasa.gov/planetary/apod?api_key=C0ehDJAti1cLdlnjQciOknJg4WMAeOBqcpOL1G4a')
-    .then(res => res.json())
-    .then(nasa => this.nasa = nasa)
+  created() {
+    this.makeAPICall();
+  },
+  methods: {
+    makeAPICall() {
+      fetch('https://api.nasa.gov/planetary/apod?api_key=C0ehDJAti1cLdlnjQciOknJg4WMAeOBqcpOL1G4a')
+      .then(res => res.json())
+      .then(nasa => this.nasa = nasa)
+    },
+    callApi() {
+      
+    }
   },
   components: {
     "image-view": ImageView
   }
-
-}
+};
 </script>
 
 <style lang="css" scoped>
 </style>
+
+
+<!-- callApi() will want to use this.selectedDate -->
